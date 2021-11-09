2f58801d7f3f9fffa8ee8a1df5a67155d66f16dc
Mehdi Aghaei <mahdiaghaei355@gmail.com>
2021-11-09 12:52:50 +0100

    INFRA: Initialize Acceptance Tests

---
+++ CoronaTracker.Core.Tests.Acceptance/Brokers/ApiTestCollection.cs
@@ -0,0 +1,9 @@
+﻿using Xunit;
+
+namespace CoronaTracker.Core.Tests.Acceptance.Brokers
+{
+    [CollectionDefinition(nameof(ApiTestCollection))]
+    public class ApiTestCollection : ICollectionFixture<CoronaTrackerApiBroker>
+    {
+    }
+}
---
+++ CoronaTracker.Core.Tests.Acceptance/Brokers/CoronaTrackerApiBroker.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Http;
+using Microsoft.AspNetCore.Mvc.Testing;
+using RESTFulSense.Clients;
+
+namespace CoronaTracker.Core.Tests.Acceptance.Brokers
+{
+    public partial class CoronaTrackerApiBroker
+    {
+        private readonly WebApplicationFactory<Startup> webApplicationFactory;
+        private readonly HttpClient httpClient;
+        private readonly IRESTFulApiFactoryClient apiFactoryClient;
+        public CoronaTrackerApiBroker()
+        {
+            this.webApplicationFactory = new WebApplicationFactory<Startup>();
+            this.httpClient = this.webApplicationFactory.CreateClient();
+            this.apiFactoryClient = new RESTFulApiFactoryClient(this.httpClient);
+        }
+
+    }
+}
