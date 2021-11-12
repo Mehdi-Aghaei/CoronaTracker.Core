912434a333f18379beef09e0320d8c61e0444208
Mehdi Aghaei <mahdiaghaei355@gmail.com>
2021-11-12 16:45:44 +0100

    BROKERS: Implement Storages Broker

---
+++ CoronaTracker.Core/Brokers/Storages/StorageBroker.cs
@@ -0,0 +1,27 @@
+﻿using EFxceptions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace CoronaTracker.Core.Brokers.Storages
+{
+    public partial class StorageBroker : EFxceptionsContext, IStorageBroker
+    {
+        private readonly IConfiguration counfiguration;
+        public StorageBroker(IConfiguration configuration)
+        {
+            this.counfiguration = configuration;
+            this.Database.Migrate();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = this.counfiguration
+                .GetConnectionString(name: "DefaultConnection");
+
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+        public override void Dispose()
+        { }
+    }
+}
