1f5755bc6d15d17d1e11d8f38293885c6ab00d3b
Mehdi Aghaei <mahdiaghaei355@gmail.com>
2021-11-12 17:13:18 +0100

    DATA: Country Model & Migrations

---
+++ CoronaTracker.Core/Brokers/Storages/StorageBroker.Countries.cs
@@ -0,0 +1,10 @@
+﻿using CoronaTracker.Core.Models.Countries;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoronaTracker.Core.Brokers.Storages
+{
+    public partial class StorageBroker
+    {
+        public DbSet<Country> Countries { get; set; }
+    }
+}
---
+++ CoronaTracker.Core/Models/Countries/Country.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CoronaTracker.Core.Models.Countries
+{
+    public class Country
+    {
+        [Key]
+        public string CountryName { get; set; }
+        public string Iso3 { get; set; }
+        public string Continent { get; set; }
+        public int Cases { get; set; }
+        public int TodayCases { get; set; }
+        public int Deaths { get; set; }
+        public int TodayDeaths { get; set; }
+        public int Recovered { get; set; }
+        public int TodayRecovered { get; set; }
+        public int Population { get; set; }
+        public int CasesPerOneMillion { get; set; }
+        public int DeathsPerOneMillion { get; set; }
+        public float RecoveredPerOneMillion { get; set; }
+        public float criticalPerOneMillion { get; set; }
+    }
+}
