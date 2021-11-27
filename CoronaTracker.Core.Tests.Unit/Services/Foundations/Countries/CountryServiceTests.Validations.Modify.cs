688201076a9be2c34b17fec9056c825ca14d01a1
Mehdi Aghaei <mahdiaghaei355@gmail.com>
2021-11-27 16:32:15 +0100

    ShouldThrowValidationExceptionOnModifyIfCountryIsInvalidAndLogItAsync -> FAIL

--- CoronaTracker.Core.Tests.Unit/Services/Foundations/Countries/CountryServiceTests.Validations.Modify.cs
+++ CoronaTracker.Core.Tests.Unit/Services/Foundations/Countries/CountryServiceTests.Validations.Modify.cs
@@ -43,5 +43,75 @@ namespace CoronaTracker.Core.Tests.Unit.Services.Foundations.Countries
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task ShouldThrowValidationExceptionOnModifyIfCountryIsInvalidAndLogItAsync(string invalidText)
+        {
+            // given 
+            var invalidCountry = new Country
+            {
+                Name = invalidText,
+                Iso3 = invalidText,
+                Continent = invalidText
+            };
+
+            var invalidCountryException = new InvalidCountryException();
+
+            invalidCountryException.AddData(
+               key: nameof(Country.Id),
+               values: "Id is required");
+
+            invalidCountryException.AddData(
+                key: nameof(Country.Name),
+                values: "Text is required");
+
+            invalidCountryException.AddData(
+                key: nameof(Country.Iso3),
+                values: "Text is required");
+
+            invalidCountryException.AddData(
+                key: nameof(Country.Continent),
+                values: "Text is required");
+
+            invalidCountryException.AddData(
+                key: nameof(Country.CreatedDate),
+                values: "Date is required");
+
+            invalidCountryException.AddData(
+                key: nameof(Country.UpdatedDate),
+                "Date is required",
+                $"Date is the same as {nameof(Country.CreatedDate)}");
+
+            var expectedCountryValidationException =
+                new CountryValidationException(invalidCountryException);
+
+            // when
+            ValueTask<Country> modifyCountryTask =
+                this.countryService.ModifyCountryAsync(invalidCountry);
+
+            // then
+            await Assert.ThrowsAsync<CountryValidationException>(() =>
+                modifyCountryTask.AsTask());
+
+            this.dateTimeBrokerMock.Verify(broker =>
+               broker.GetCurrentDateTimeOffset(),
+                   Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedCountryValidationException))),
+                        Times.Once());
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateCountryAsync(It.IsAny<Country>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls(); 
+        }
     }
 }
