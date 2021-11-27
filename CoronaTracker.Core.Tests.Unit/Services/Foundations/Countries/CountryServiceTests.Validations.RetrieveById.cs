448135b6119974e73421d7efce9a8b8db07b9c4b
Mehdi Aghaei <mahdiaghaei355@gmail.com>
2021-11-27 11:58:01 +0100

    ShouldThrowValidationExceptionOnRetrieveByIdIfIdIsInvalidAndLogItAsync -> FAIL

---
+++ CoronaTracker.Core.Tests.Unit/Services/Foundations/Countries/CountryServiceTests.Validations.RetrieveById.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CoronaTracker.Core.Models.Countries;
+using CoronaTracker.Core.Models.Countries.Exceptions;
+using Moq;
+using Xunit;
+
+namespace CoronaTracker.Core.Tests.Unit.Services.Foundations.Countries
+{
+    public partial class CountryServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnRetrieveByIdIfIdIsInvalidAndLogItAsync() 
+        {
+            // given
+            Guid invalidCountryId = Guid.Empty;
+
+            var invalidCountryException =
+                new InvalidCountryException();
+
+            invalidCountryException.AddData(
+                key: nameof(Country.Id),
+                values: "Id is required");
+
+            var expectedCountryValidationException =
+                new CountryValidationException(invalidCountryException);
+
+            // when
+            ValueTask<Country> retrieveByIdCountryTask =
+                this.countryService.RetrieveCountryByIdAsync(invalidCountryId);
+
+            // then
+            await Assert.ThrowsAsync<CountryValidationException>(() =>
+                retrieveByIdCountryTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedCountryValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectCountryByIdAsync(It.IsAny<Guid>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
