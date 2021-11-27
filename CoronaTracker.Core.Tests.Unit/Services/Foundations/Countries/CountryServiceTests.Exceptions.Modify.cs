222cef6ea51dc913b34e7efd649e85aa13debb62
Mehdi Aghaei <mahdiaghaei355@gmail.com>
2021-11-27 17:12:03 +0100

    ShouldThrowCriticalDependencyExceptionOnModifyIfSqlErrorOccursAndLogItAsync -> PASS

---
+++ CoronaTracker.Core.Tests.Unit/Services/Foundations/Countries/CountryServiceTests.Exceptions.Modify.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CoronaTracker.Core.Models.Countries;
+using CoronaTracker.Core.Models.Countries.Exceptions;
+using Microsoft.Data.SqlClient;
+using Moq;
+using Xunit;
+
+namespace CoronaTracker.Core.Tests.Unit.Services.Foundations.Countries
+{
+    public partial class CountryServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowCriticalDependencyExceptionOnModifyIfSqlErrorOccursAndLogItAsync()
+        {
+            // given
+            Country someCountry = CreateRandomCountry();
+            SqlException sqlException = GetSqlException();
+
+            var failedCountryStorageException =
+                new FailedCountryStorageException(sqlException);
+
+            var expectedCountryDependencyException =
+                new CountryDependencyException(failedCountryStorageException);
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTimeOffset())
+                    .Throws(sqlException);
+
+            // then
+            ValueTask<Country> addCountryTask =
+                this.countryService.ModifyCountryAsync(someCountry);
+
+            // then
+            await Assert.ThrowsAsync<CountryDependencyException>(() =>
+                addCountryTask.AsTask());
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffset(),
+                    Times.Once);
+            this.storageBrokerMock.Verify(broker =>
+             broker.SelectCountryByIdAsync(someCountry.Id),
+                 Times.Never);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedCountryDependencyException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateCountryAsync(It.IsAny<Country>()),
+                    Times.Never);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
