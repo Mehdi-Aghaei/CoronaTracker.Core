86291cd975bae7575a474c7221e170523e189fff
Mehdi Aghaei <mahdiaghaei355@gmail.com>
2021-12-01 07:44:05 +0100

    PROCESSING: Upsert Country

---
+++ CoronaTracker.Core/Services/Processings/Countries/CountryProcessingService.cs
@@ -0,0 +1,31 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using CoronaTracker.Core.Brokers.Loggings;
+using CoronaTracker.Core.Models.Countries;
+using CoronaTracker.Core.Services.Foundations.Countries;
+
+namespace CoronaTracker.Core.Services.Processings.Countries
+{
+    public class CountryProcessingService : ICountryProcessingService
+    {
+        private readonly ICountryService countryService;
+        private readonly ILoggingBroker loggingBroker;
+
+        public CountryProcessingService(
+            ICountryService countryService, 
+            ILoggingBroker loggingBroker)
+        {
+            this.countryService = countryService;
+            this.loggingBroker = loggingBroker;
+        }
+
+        public ValueTask<Country> UpsertCountryAsync(Country country)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
