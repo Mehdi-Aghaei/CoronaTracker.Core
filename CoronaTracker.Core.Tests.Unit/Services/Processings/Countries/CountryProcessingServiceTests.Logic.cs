3f39afc5bd70c10f7ffc53d26477d897fd4da798
Hassan Habib <hassanhabib@live.com>
2021-11-30 22:56:40 -0800

    ShouldAddCountryIfNotExistAsync -> FAIL

---
+++ CoronaTracker.Core.Tests.Unit/Services/Processings/Countries/CountryProcessingServiceTests.Logic.cs
@@ -0,0 +1,60 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using System.Linq;
+using System.Threading.Tasks;
+using CoronaTracker.Core.Models.Countries;
+using FluentAssertions;
+using Force.DeepCloner;
+using Moq;
+using Xunit;
+
+namespace CoronaTracker.Core.Tests.Unit.Services.Processings.Countries
+{
+    public partial class CountryProcessingServiceTests
+    {
+        [Fact]
+        public async Task ShouldAddCountryIfNotExistAsync()
+        {
+            // given
+            IQueryable<Country> randomCountries =
+                CreateRandomCountries();
+
+            IQueryable<Country> retrievedCountries =
+                randomCountries;
+
+            Country randomCountry = CreateRandomCountry();
+            Country inputCountry = randomCountry;
+            Country addedCountry = inputCountry;
+            Country expectedCountry = addedCountry.DeepClone();
+
+            this.countryServiceMock.Setup(service =>
+                service.RetrieveAllCountries())
+                    .Returns(retrievedCountries);
+
+            this.countryServiceMock.Setup(service =>
+                service.AddCountryAsync(inputCountry))
+                    .ReturnsAsync(addedCountry);
+
+            // when
+            Country actualCountry = await this.countryProcessingService
+                .UpsertCountryAsync(inputCountry);
+
+            // then
+            actualCountry.Should().BeEquivalentTo(expectedCountry);
+
+            this.countryServiceMock.Verify(service =>
+                service.RetrieveAllCountries(),
+                    Times.Once);
+
+            this.countryServiceMock.Verify(service =>
+                service.AddCountryAsync(inputCountry),
+                    Times.Once);
+
+            this.countryServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ CoronaTracker.Core.Tests.Unit/Services/Processings/Countries/CountryProcessingServiceTests.cs
@@ -0,0 +1,55 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using System;
+using System.Linq;
+using CoronaTracker.Core.Brokers.Loggings;
+using CoronaTracker.Core.Models.Countries;
+using CoronaTracker.Core.Services.Foundations.Countries;
+using CoronaTracker.Core.Services.Processings.Countries;
+using Moq;
+using Tynamix.ObjectFiller;
+
+namespace CoronaTracker.Core.Tests.Unit.Services.Processings.Countries
+{
+    public partial class CountryProcessingServiceTests
+    {
+        private readonly Mock<ICountryService> countryServiceMock;
+        private readonly Mock<ILoggingBroker> loggingBrokerMock;
+        private readonly ICountryProcessingService countryProcessingService;
+
+        public CountryProcessingServiceTests()
+        {
+            this.countryServiceMock = new Mock<ICountryService>();
+            this.loggingBrokerMock = new Mock<ILoggingBroker>();
+
+            this.countryProcessingService = new CountryProcessingService(
+                countryService: this.countryServiceMock.Object,
+                loggingBroker: this.loggingBrokerMock.Object);
+        }
+
+        private static Country CreateRandomCountry() =>
+            CreateCountryFiller().Create();
+
+        private static IQueryable<Country> CreateRandomCountries() =>
+            CreateCountryFiller().Create(count: GetRandomNumber()).AsQueryable();
+
+        private static int GetRandomNumber() =>
+            new IntRange(min: 2, max: 10).GetValue();
+
+        private static DateTimeOffset GetRandomDateTimeOffset() =>
+            new DateTimeRange(earliestDate: new DateTime()).GetValue();
+
+        private static Filler<Country> CreateCountryFiller()
+        {
+            var filler = new Filler<Country>();
+
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(GetRandomDateTimeOffset());
+
+            return filler;
+        }
+    }
+}
