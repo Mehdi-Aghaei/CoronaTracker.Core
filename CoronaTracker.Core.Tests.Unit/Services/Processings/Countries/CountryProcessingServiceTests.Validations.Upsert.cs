1aae6e2d8e50751488cdd050d0e7da50a7962e4d
Mehdi Aghaei <mahdiaghaei355@gmail.com>
2021-12-07 21:08:51 +0100

    ShouldThrowValidationExceptionOnUpsertIfCountryIdIsInvalidAndLogItAsync -> FAIL

--- CoronaTracker.Core.Tests.Unit/Services/Processings/Countries/CountryProcessingServiceTests.Validations.Upsert.cs
+++ CoronaTracker.Core.Tests.Unit/Services/Processings/Countries/CountryProcessingServiceTests.Validations.Upsert.cs
@@ -3,6 +3,7 @@
 // FREE TO USE TO CONNECT THE WORLD
 // ---------------------------------------------------------------
 
+using System;
 using System.Threading.Tasks;
 using CoronaTracker.Core.Models.Countries;
 using CoronaTracker.Core.Models.Processings.Countries.Exceptions;
@@ -39,6 +40,52 @@ namespace CoronaTracker.Core.Tests.Unit.Services.Processings.Countries
                 broker.LogError(It.Is(SameExceptionAs(
                     expectedCountryProcessingValidationException))),
                         Times.Once);
+
+            this.countryServiceMock.Verify(service =>
+                service.RetrieveAllCountries(),
+                    Times.Never);
+
+            this.countryServiceMock.Verify(service =>
+                service.AddCountryAsync(It.IsAny<Country>()),
+                    Times.Never);
+
+            this.countryServiceMock.Verify(service =>
+                service.ModifyCountryAsync(It.IsAny<Country>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.countryServiceMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnUpsertIfCountryIdIsInvalidAndLogItAsync()
+        {
+            // given
+            var invalidCountry = CreateRandomCountry();
+            invalidCountry.Id = Guid.Empty;
+
+            var invalidCountryProcessingException =
+                new InvalidCountryProcessingException();
+
+            invalidCountryProcessingException.AddData(
+                key: nameof(Country.Id),
+                values: "Id is required");
+
+            var expectedCountryProcessingValidationException=
+                new CountryProcessingValidationException(invalidCountryProcessingException);
+
+            // when
+            ValueTask<Country> upsertCountryTask =
+                this.countryProcessingService.UpsertCountryAsync(invalidCountry);
+
+            // then
+            await Assert.ThrowsAsync<CountryProcessingValidationException>(() =>
+                upsertCountryTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+              broker.LogError(It.Is(SameExceptionAs(
+                  expectedCountryProcessingValidationException))),
+                      Times.Once);
 
             this.countryServiceMock.Verify(service =>
                 service.RetrieveAllCountries(),
---
+++ CoronaTracker.Core/Models/Processings/Countries/Exceptions/InvalidCountryProcessingException.cs
@@ -0,0 +1,16 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using Xeptions;
+
+namespace CoronaTracker.Core.Models.Processings.Countries.Exceptions
+{
+    public class InvalidCountryProcessingException : Xeption
+    {
+        public InvalidCountryProcessingException()
+            : base(message: "Invalid country, Please correct the errors and try again.")
+        { }
+    }
+}
