2a4436a337b78535b05aed2ab7ecb88b80f0b91f
Mehdi Aghaei <mahdiaghaei355@gmail.com>
2022-02-23 08:02:50 +0100

    BROKERS: Initialize Country Queue Broker.

---
+++ CoronaTracker.Core/Brokers/Queues/QueueBroker.cs
@@ -0,0 +1,33 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using System;
+using Microsoft.Azure.ServiceBus;
+using Microsoft.Extensions.Configuration;
+
+namespace CoronaTracker.Core.Brokers.Queues
+{
+    public partial class QueueBroker : IQueueBroker
+    {
+        private readonly IConfiguration configuration;
+
+        public QueueBroker(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+            InitializeClients();
+        }
+
+        private void InitializeClients() =>
+            this.CountryQueue = GetQueueClient(nameof(CountryQueue));
+
+        private IQueueClient GetQueueClient(string queueName)
+        {
+            string queueConnectionString =
+                this.configuration.GetConnectionString("ServiceBusConnection");
+
+            return new QueueClient(queueConnectionString, queueName);
+        }
+    }
+}
