c1c015f5dc0340b9596beae7e3fea1c96c66bdf1
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2022-02-27 22:20:26 -0800

    ShouldThrowServiceExceptionOnAddIfServiceErrorOccursAndLogItAsync -> FAIL

---
+++ CoronaTracker.Core/Models/CountryEvents/Exceptions/FailedCountryEventServiceException.cs
@@ -0,0 +1,17 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using System;
+using Xeptions;
+
+namespace CoronaTracker.Core.Models.CountryEvents.Exceptions
+{
+    public class FailedCountryEventServiceException : Xeption
+    {
+        public FailedCountryEventServiceException(Exception innerException)
+            : base(message: "Failed country event service error occured, please contact support", innerException)
+        { }
+    }
+}
