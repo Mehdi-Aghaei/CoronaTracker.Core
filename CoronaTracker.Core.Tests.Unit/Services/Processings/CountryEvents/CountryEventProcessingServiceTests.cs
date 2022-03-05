aa49c223b5cb76062a9d9dc2267d664ef810b2f7
Mehdi Aghaei <mahdiaghaei355@gmail.com>
2022-03-05 12:10:17 +0100

    ShouldAddCountryEventAsync -> FAIL

---
+++ CoronaTracker.Core.Tests.Unit/Services/Processings/CountryEvents/CountryEventProcessingServiceTests.cs
@@ -0,0 +1,43 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CoronaTracker.Core.Brokers.Loggings;
+using CoronaTracker.Core.Models.CountryEvents;
+using CoronaTracker.Core.Services.Foundations.CountryEvents;
+using CoronaTracker.Core.Services.Processings.CountryEvents;
+using Moq;
+using Tynamix.ObjectFiller;
+
+namespace CoronaTracker.Core.Tests.Unit.Services.Processings.CountryEvents
+{
+    public partial class CountryEventProcessingServiceTests
+    {
+        private readonly Mock<ICountryEventService> countryEventServiceMock;
+        private readonly Mock<ILoggingBroker> loggingBrokerMock;
+        private readonly ICountryEventProcessingService countryEventProcessingService;
+
+        public CountryEventProcessingServiceTests()
+        {
+            this.countryEventServiceMock = new Mock<ICountryEventService>();
+            this.loggingBrokerMock = new Mock<ILoggingBroker>();
+
+            this.countryEventProcessingService = new CountryEventProcessingService(
+                countryEventService: countryEventServiceMock.Object,
+                loggingBroker: loggingBrokerMock.Object);
+
+        }
+
+        private static CountryEvent CreateRandomCountryEvent() =>
+            CreateCountryEventFiller().Create();
+
+        private static Filler<CountryEvent> CreateCountryEventFiller() =>
+            new Filler<CountryEvent>();
+    }
+}
---
+++ CoronaTracker.Core/Services/Processings/CountryEvents/CountryEventProcessingService.cs
@@ -0,0 +1,28 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using CoronaTracker.Core.Brokers.Loggings;
+using CoronaTracker.Core.Models.CountryEvents;
+using CoronaTracker.Core.Services.Foundations.CountryEvents;
+
+namespace CoronaTracker.Core.Services.Processings.CountryEvents
+{
+    public class CountryEventProcessingService : ICountryEventProcessingService
+    {
+        private readonly ICountryEventService countryEventService;
+        private readonly ILoggingBroker loggingBroker;
+
+        public CountryEventProcessingService(ICountryEventService countryEventService, ILoggingBroker loggingBroker)
+        {
+            this.countryEventService = countryEventService;
+            this.loggingBroker = loggingBroker;
+        }
+
+        public async ValueTask<CountryEvent> AddCountryEventAsync(CountryEvent countryEvent) =>
+            throw new NotImplementedException();
+    }
+}
