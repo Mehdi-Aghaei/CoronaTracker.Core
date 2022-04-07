ceb2ad2dea6acf97f2928347f91a11a1d21b2b6c
Mehdi Aghaei <mahdiaghaei355@gmail.com>
2022-04-07 03:12:31 +0200

    ShouldThrowDependencyExceptionOnRetrieveAllIfDependencyErrorOccursAndLogIt -> PASS

---
+++ CoronaTracker.Core.Tests.Unit/Services/Processings/Countries/CountryProcessingServiceTests.Exceptions.RetrieveAll.cs
@@ -0,0 +1,54 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using CoronaTracker.Core.Models.Countries;
+using CoronaTracker.Core.Models.Processings.Countries.Exceptions;
+using Moq;
+using Xeptions;
+using Xunit;
+
+namespace CoronaTracker.Core.Tests.Unit.Services.Processings.Countries
+{
+    public partial class CountryProcessingServiceTests
+    {
+        [Theory]
+        [MemberData(nameof(DependencyExceptions))]
+        public void ShouldThrowDependencyExceptionOnRetrieveAllIfDependencyErrorOccursAndLogIt(
+           Xeption dependencyException)
+        {
+            // given
+            var someCountry = CreateRandomCountries();
+
+            var expectedCountryProcessingDependencyException =
+                new CountryProcessingDependencyException(
+                    dependencyException.InnerException as Xeption);
+
+            this.countryServiceMock.Setup(service =>
+                service.RetrieveAllCountries())
+                    .Throws(dependencyException);
+
+            // when
+            Action retrieveAllCountriesAction = () =>
+                this.countryProcessingService.RetrieveAllCountries();
+
+            // then
+            Assert.Throws<CountryProcessingDependencyException>(retrieveAllCountriesAction);
+
+            this.countryServiceMock.Verify(service =>
+                service.RetrieveAllCountries(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedCountryProcessingDependencyException))),
+                        Times.Once);
+
+            this.countryServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ CoronaTracker.Core.Tests.Unit/Services/Processings/Countries/CountryProcessingServiceTests.Logic.RetrieveAll.cs
@@ -0,0 +1,45 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using System.Linq;
+using System.Threading.Tasks;
+using CoronaTracker.Core.Models.Countries;
+using FluentAssertions;
+using Force.DeepCloner;
+using Moq;
+using Xunit;
+
+namespace CoronaTracker.Core.Tests.Unit.Services.Processings.Countries
+{
+    public partial class CountryProcessingServiceTests
+    {
+        [Fact]
+        public void ShouldRetrieveAllCountries()
+        {
+            // given
+            IQueryable<Country> randomCountries = CreateRandomCountries();
+            IQueryable<Country> storageCountries = randomCountries;
+            IQueryable<Country> expectedCountries = storageCountries;
+
+            this.countryServiceMock.Setup(service =>
+                service.RetrieveAllCountries())
+                    .Returns(storageCountries);
+
+            // when
+            IQueryable<Country> actualCountries = 
+                this.countryProcessingService.RetrieveAllCountries();
+
+            // then
+            actualCountries.Should().BeEquivalentTo(expectedCountries);
+
+            this.countryServiceMock.Verify(service =>
+                service.RetrieveAllCountries(),
+                    Times.Once);
+
+            this.countryServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
