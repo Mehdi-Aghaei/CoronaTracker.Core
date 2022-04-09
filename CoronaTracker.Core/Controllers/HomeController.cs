fb682e5984293f2efe6c3ae31f597377463361ac
Mehdi Aghaei <mahdiaghaei355@gmail.com>
2022-04-09 19:47:13 +0200

    CONTROLLERS: Add Sample controller

---
+++ CoronaTracker.Core/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoronaTracker.Core.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+
+        [HttpGet]
+        public ActionResult<string> Get() =>
+            Ok("Hello,everyone");
+    }
+}
