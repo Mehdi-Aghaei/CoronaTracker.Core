5782cd9ab9cd6f52762f5dbe9ba73ffdab1574c9
Mehdi Aghaei <mahdiaghaei355@gmail.com>
2022-04-20 13:03:27 +0200

    ACCEPTANCE : Home API Test

---
+++ CoronaTracker.Core.Tests.Acceptance/Apis/Home/HomeApiTests.Logic.cs
@@ -0,0 +1,29 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace CoronaTracker.Core.Tests.Acceptance.Apis.Home
+{
+    public partial class HomeApiTests
+    {
+        [Fact]
+        public async Task ShouldReturnHomeMessageAsync()
+        {
+            // given
+            string expectedHomeMessage =
+                "Hi dear, you have to check countries' API :)";
+
+            // when
+            string actualHomeMessage =
+                await this.coronaTrackerApiBroker.GetHomeMessageAsync();
+
+            // then
+            actualHomeMessage.Should().BeEquivalentTo(expectedHomeMessage);
+        }
+    }
+}
